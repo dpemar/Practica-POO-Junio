eb19168e119bee6086fd51a2895a856674aaa709
dpemar <dpemar@hotmail.com>
2018-05-22 23:26:52 +0200

    Actualizando Bolsa

--- src/es/poo/bolsa/BolsaDeValores.java
+++ src/es/poo/bolsa/BolsaDeValores.java
@@ -1,5 +1,27 @@
 package es.poo.bolsa;
 
+import java.util.HashSet;
+
 public class BolsaDeValores {
 
+	private String nombreBolsa;
+	private HashSet<Empresa> listaEmpresas = new HashSet<Empresa>();
+
+	// Añadir empresa
+	public void añadirEmpresa(Empresa empresa) {
+		this.listaEmpresas.add(empresa);
+	}
+
+	// Eliminar empresa
+	public void eliminarEmpresa(Empresa empresa) {
+		this.listaEmpresas.remove(empresa);
+	}
+
+	// Listado estado empresas
+	public void listarEstadoEmpresas() {
+		for (Empresa empresa : listaEmpresas) {
+			System.out.println("Valor actual accion: " + empresa.getValorAccionActual());
+			System.out.println("Incremento: " + empresa.getIncremento());
+		}
+	}
 }
--- src/es/poo/bolsa/Empresa.java
+++ src/es/poo/bolsa/Empresa.java
@@ -2,4 +2,51 @@ package es.poo.bolsa;
 
 public class Empresa {
 
+	private String nombreEmpresa;
+	private int valorAccionActual;
+	private int valorAccionPrevio;
+	private int incremento = (valorAccionPrevio - valorAccionActual);
+
+	// Constructor
+	public Empresa(String nombreEmpresa, int valorAccionActual, int valorAccionPrevio) {
+		super();
+		this.nombreEmpresa = nombreEmpresa;
+		this.valorAccionActual = valorAccionActual;
+		this.valorAccionPrevio = valorAccionPrevio;
+	}
+
+	// Get&Set
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+
+	public int getValorAccionActual() {
+		return valorAccionActual;
+	}
+
+	public void setValorAccionActual(int valorAccionActual) {
+		this.valorAccionActual = valorAccionActual;
+	}
+
+	public int getValorAccionPrevio() {
+		return valorAccionPrevio;
+	}
+
+	public void setValorAccionPrevio(int valorAccionPrevio) {
+		this.valorAccionPrevio = valorAccionPrevio;
+	}
+
+	public int getIncremento() {
+		return incremento;
+	}
+
+	public void setIncremento(int incremento) {
+		this.incremento = incremento;
+	}
+	
+
 }
